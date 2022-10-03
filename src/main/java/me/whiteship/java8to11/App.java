1a851cc5d32c6b6ce9a10e17a5618a2efd3ae2d9
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-10-04 01:09:39 +0900

    feat: Optional 강의 실습

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -8,19 +8,31 @@ import java.util.stream.Stream;
 public class App {
 
     public static void main(String[] arg){
-//        List<OnlineClass> springClass = new ArrayList<>();
-//        springClass.add(new OnlineClass(1,"spring boot",true));
+        List<OnlineClass> springClass = new ArrayList<>();
+        springClass.add(new OnlineClass(1,"spring boot",true));
 //        springClass.add(new OnlineClass(2,"spring data jpa",true));
 //        springClass.add(new OnlineClass(3,"spring mvc",false));
 //        springClass.add(new OnlineClass(4,"spring core",false));
-//        springClass.add(new OnlineClass(5,"rest api development",false));
-        for (Direction direction:Direction.values()){
-            System.out.println(direction.name()+"/"+direction.getValue() + "/" + direction.getSymbol() + "/"+ direction.ordinal());
-        }
-        innerClass aClass = new innerClass();
-        innerClass.ZipAddress zipAddress = new innerClass.ZipAddress();
-        aClass.name = "aaa";
-        aClass.zipAddress.printName();
+        springClass.add(new OnlineClass(5,"rest api development",false));
+        Optional<OnlineClass> spring = springClass.stream()
+                .filter(onlineClass -> onlineClass.getTitle().startsWith("spring"))
+                .findFirst();
+        boolean present = spring.isPresent();
+        System.out.println(present);
 
+        spring.ifPresent(onlineClass -> System.out.println(onlineClass.getTitle()));
+
+        OnlineClass onlineClass = spring.orElseGet(App::createNewClass);
+        System.out.println(onlineClass.getTitle());
+
+        Optional<OnlineClass> onlineClass2 = spring.filter(onlineClass1 -> onlineClass1.getId() > 10);
+        System.out.println(onlineClass2.isPresent());
+
+        Optional<Integer> onlineClass3 = spring.map(OnlineClass::getId);
+        System.out.println(onlineClass3.get());
+    }
+
+    private static OnlineClass createNewClass(){
+        return new OnlineClass(0,"New Class",false);
     }
 }
