c90061b840cf67de046c707358562414190d7ba4
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-10-06 23:48:55 +0900

    feat: Instant Time API 활용

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,5 +1,8 @@
 package me.whiteship.java8to11;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.*;
 import java.util.function.*;
 import java.util.stream.Collectors;
@@ -7,32 +10,14 @@ import java.util.stream.Stream;
 
 public class App {
 
-    public static void main(String[] arg){
-        List<OnlineClass> springClass = new ArrayList<>();
-        springClass.add(new OnlineClass(1,"spring boot",true));
-//        springClass.add(new OnlineClass(2,"spring data jpa",true));
-//        springClass.add(new OnlineClass(3,"spring mvc",false));
-//        springClass.add(new OnlineClass(4,"spring core",false));
-        springClass.add(new OnlineClass(5,"rest api development",false));
-        Optional<OnlineClass> spring = springClass.stream()
-                .filter(onlineClass -> onlineClass.getTitle().startsWith("spring"))
-                .findFirst();
-        boolean present = spring.isPresent();
-        System.out.println(present);
+    public static void main(String[] arg) throws InterruptedException {
+        Instant instant = Instant.now();
+        System.out.println(instant); // 기준시 UTC
+        System.out.println(instant.atZone(ZoneId.of("UTC"))); // 기준시 UTC
 
-        spring.ifPresent(onlineClass -> System.out.println(onlineClass.getTitle()));
-
-        OnlineClass onlineClass = spring.orElseGet(App::createNewClass);
-        System.out.println(onlineClass.getTitle());
-
-        Optional<OnlineClass> onlineClass2 = spring.filter(onlineClass1 -> onlineClass1.getId() > 10);
-        System.out.println(onlineClass2.isPresent());
-
-        Optional<Integer> onlineClass3 = spring.map(OnlineClass::getId);
-        System.out.println(onlineClass3.get());
-    }
-
-    private static OnlineClass createNewClass(){
-        return new OnlineClass(0,"New Class",false);
+        ZoneId zone = ZoneId.systemDefault();
+        System.out.println(zone);
+        ZonedDateTime zonedDateTime = instant.atZone(zone);
+        System.out.println(zonedDateTime);
     }
 }
