780fa2bd02c0b085ae39cd84a5474ebe6658ec3a
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-10-07 20:48:43 +0900

    feat: Callable과 future의 invoke를 통한 병렬 처리

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,26 +1,40 @@
 package me.whiteship.java8to11;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class App {
 
-    public static void main(String[] arg){
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
+    public static void main(String[] arg) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
         Callable<String> hello = ()-> {
             Thread.sleep(2000L);
-            return "hellow";
+            return "Hello";
         };
 
-        Future<String> submit = executorService.submit(hello);
-        System.out.println(submit.isDone());
-        System.out.println("Started!");
-        submit.cancel(true);
-//        submit.get(); // 블락킹 콜
-        System.out.println(submit.isDone());
-        System.out.println("End!!");
+        Callable<String> java = ()-> {
+            Thread.sleep(3000L);
+            return "Java";
+        };
+
+        Callable<String> keesun = ()-> {
+            Thread.sleep(1000L);
+            return "Keesun";
+        };
+//        String s = executorService.invokeAny(Arrays.asList(hello,java,keesun));
+//        System.out.println(s);
+
+        List<Future<String>> futures = executorService.invokeAll(Arrays.asList(hello,java,keesun));
+        for(Future<String> future : futures){
+            System.out.println(future.get());
+        }
+
         executorService.shutdown();
     }
 
