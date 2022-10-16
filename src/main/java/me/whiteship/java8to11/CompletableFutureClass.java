706ba386bfa25e80a6022d0a988ab6a245ed55b7
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-10-17 00:05:57 +0900

    feat: CompletableFuture 동시 동작 처리

--- src/main/java/me/whiteship/java8to11/CompletableFutureClass.java
+++ src/main/java/me/whiteship/java8to11/CompletableFutureClass.java
@@ -1,6 +1,5 @@
 package me.whiteship.java8to11;
 
-import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -10,23 +9,45 @@ public class CompletableFutureClass {
     public static void run() throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(4);
         CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Hello start");
+            try {
+                Thread.sleep(5000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("Hello "+ Thread.currentThread().getName());
             return "hello";
-        },executorService).thenApply(s -> {
-            System.out.println("Hello2 :"+Thread.currentThread().getName());
-            return s.toLowerCase(Locale.ROOT);
         });
 
-        CompletableFuture<Void> future2 = CompletableFuture.supplyAsync(() -> {
-            System.out.println("Hello3 "+ Thread.currentThread().getName());
-            return "hello3";
-        }).thenRun(() -> {
-            System.out.println("End future2");
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Hello2 start");
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello2 "+ Thread.currentThread().getName());
+            return "hello2";
         });
+
+
+//        CompletableFuture<String> future2 = future.thenCompose(CompletableFutureClass::getworld);
 
         System.out.println("Start");
-        System.out.println("result :" + future.get());
-        future2.get();
+//        System.out.println("result :" + future.get());
+//        System.out.println("result2 :" + future2.get());
+//
+        CompletableFuture<Void> finalFuture = CompletableFuture.allOf(future,future2)
+                .thenAccept(System.out::println);
+        finalFuture.get();
         System.out.println("End");
     }
+
+    private static CompletableFuture<String> getworld(String message) {
+        return CompletableFuture.supplyAsync(() -> {
+            System.out.println("Helloworld "+ Thread.currentThread().getName());
+            return message+"hello3";
+        });
+    }
+
 }
