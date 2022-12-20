4df8cbab084bf87e56978170cd57028f7ed21e6e
윤홍찬 <ychqwertyuio@gmail.com>
2022-12-20 22:34:33 +0900

    feat: Byte Code 조작 관련 기능 추가

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,7 +1,13 @@
 package me.whiteship.java8to11;
 
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FixedValue;
 
 @MyAnnotation
 @MyAnnotation(20)
@@ -11,17 +17,24 @@ public class App {
     public static void main(String[] arg) throws ExecutionException, InterruptedException {
 //        CompletableFutureClass.run();
 //        ParallelSort.run();
-        FeelsLikeChicken<String> names = new FeelsLikeChicken<>();
-        names.print("aaa");
-        MyAnnotation[] myAnnotations = App.class.getAnnotationsByType(MyAnnotation.class);
-        Arrays.stream(myAnnotations).forEach(myAnnotation -> {
-            System.out.println("value = "+myAnnotation.value());
-        });
-        Myannotations annotationsByType = App.class.getAnnotation(Myannotations.class);
-        Arrays.stream(annotationsByType.value()).forEach(myAnnotation -> {
-            System.out.println("value = "+myAnnotation.value());
-        });
+//        FeelsLikeChicken<String> names = new FeelsLikeChicken<>();
+//        names.print("aaa");
+//        MyAnnotation[] myAnnotations = App.class.getAnnotationsByType(MyAnnotation.class);
+//        Arrays.stream(myAnnotations).forEach(myAnnotation -> {
+//            System.out.println("value = "+myAnnotation.value());
+//        });
+//        Myannotations annotationsByType = App.class.getAnnotation(Myannotations.class);
+//        Arrays.stream(annotationsByType.value()).forEach(myAnnotation -> {
+//            System.out.println("value = "+myAnnotation.value());
+//        });
 
-
+//        try {
+//            new ByteBuddy().redefine(Moja.class)
+//                    .method(named("pullOut")).intercept(FixedValue.value("Rabbit!"))
+//                    .make().saveIn(new File("/Users/윤홍찬/Desktop/공부/java8/target/classes/"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        System.out.println(new Moja().pullOut());
     }
 }
---
+++ src/main/java/me/whiteship/java8to11/Moja.java
@@ -0,0 +1,7 @@
+package me.whiteship.java8to11;
+
+public class Moja {
+    public String pullOut(){
+        return "";
+    }
+}
